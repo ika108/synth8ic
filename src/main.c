f81f2beb3e36aa6d60845cf2019c6d1b2fe5e736
Sven SZEJWACH <sven@netmonk.org>
2013-02-10 10:16:25 +0100

    Testing autotools skeleton

---
+++ src/main.c
@@ -0,0 +1,10 @@
+// For now, this file is just a stub to play with autotools...
+
+#include <config.h>
+#include <stdio.h>
+
+int main(void) {
+	puts("Hello World !");
+	puts("This is " PACKAGE_STRING ".");
+	return(0);
+}
